94a68e76f536bde44e4d4d6d27d56964d742c551
Albert Rocamora <arocamop@everis.com>
2020-02-19 20:51:01 +0100

    Modificación 2
    Nuevo titulo

--- src/helloWorld/index.java
+++ src/helloWorld/index.java
@@ -5,6 +5,7 @@ public class index {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World2!");
+		System.out.println("Nuevo titulo!");
 
 	}
 
