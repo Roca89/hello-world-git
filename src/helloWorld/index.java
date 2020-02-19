9e358d5824e29820829440ee06de24bef65b6c84
Albert Rocamora <arocamop@everis.com>
2020-02-19 20:42:47 +0100

    Revert "Modificación 2"
    This reverts commit bd245572e064fcf8128ec310f51fc06ba4092726.

--- src/helloWorld/index.java
+++ src/helloWorld/index.java
@@ -5,7 +5,6 @@ public class index {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World2!");
-		System.out.println("Nuevo titulo!");
 
 	}
 
