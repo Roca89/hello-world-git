14c667cb4764e46ec01c35120fb91469383ea42b
Albert <arocamop@everis.com>
2020-03-22 19:25:25 +0100

    Segunda modificación Jordi
    Modificaciones Jordi

--- src/helloWorld/index.java
+++ src/helloWorld/index.java
@@ -10,6 +10,7 @@ public class index {
 		System.out.println("Modificaciones Albert");
 		System.out.println("Modificaciones Jordi");
 		System.out.println("Segundas modificaciones Albert");
+		System.out.println("Modificaciones segundas de Jordi");
 	
 
 
